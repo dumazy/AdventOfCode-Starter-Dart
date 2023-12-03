c62fe2021917f6704534ad30cffbc2b298525260
Fré Dumazy <dumazy@gmail.com>
2023-12-03 16:59:41 +0100

    Add Day 3

--- solutions/day03.dart
+++ solutions/day03.dart
@@ -1,23 +1,123 @@
+import 'package:quiver/pattern.dart';
+
 import '../utils/index.dart';
+
+/// numbers and anything other than a dot (.)
+final engineMapRegExp = RegExp('([0-9]+|[^.])');
+final numberRegExp = RegExp('[0-9]+');
 
 class Day03 extends GenericDay {
   Day03() : super(3);
 
-  @override
-  parseInput() {
+  List<NumberField> numbers = [];
+  List<SymbolField> symbols = [];
 
+  @override
+  void parseInput() {
+    numbers.clear();
+    symbols.clear();
+    final indexedLines = input.getPerLine().indexed;
+    for (final (y, line) in indexedLines) {
+      final matches = engineMapRegExp.allMatches(line);
+      for (final match in matches) {
+        final x = match.start;
+        final value = match.group(0)!;
+        final isNumber = numberRegExp.hasMatch(value);
+        if (!isNumber) {
+          final field = SymbolField(x, y, value);
+          symbols.add(field);
+          continue;
+        }
+        final number = int.parse(value);
+        final field = NumberField(x, y, number);
+        numbers.add(field);
+      }
+    }
   }
 
   @override
   int solvePart1() {
-
-    return 0;
+    parseInput();
+    final engineParts = numbers.where((numberField) {
+      final adjacentFields = numberField.adjecentFields;
+      return adjacentFields.any((adjacentField) {
+        return symbols.any((symbolField) {
+          return symbolField.x == adjacentField.$1 &&
+              symbolField.y == adjacentField.$2;
+        });
+      });
+    });
+    final engineNumbers = engineParts.map((e) => e.value);
+    return engineNumbers.reduce((a, b) => a + b);
   }
 
   @override
   int solvePart2() {
+    parseInput();
+    var gearRatio = 0;
+    final gears = symbols.where((symbolField) => symbolField.value == '*');
+    for (final gear in gears) {
+      final adjacentNumbers = numbers.where((number) {
+        return number.adjecentFields.any((adjacentField) {
+          return adjacentField.$1 == gear.x && adjacentField.$2 == gear.y;
+        });
+      });
+      if (adjacentNumbers.length == 2) {
+        gearRatio += adjacentNumbers.first.value * adjacentNumbers.last.value;
+      }
+    }
 
-    return 0;
+    return gearRatio;
   }
 }
 
+class Field<T> {
+  Field(this.x, this.y, this.value);
+
+  final int x;
+  final int y;
+  final T value;
+
+  int get width => 1;
+  int get height => 1;
+
+  @override
+  String toString() {
+    return 'Field<$T>(x: $x, y: $y, value: $value)';
+  }
+}
+
+class NumberField extends Field<int> {
+  NumberField(super.x, super.y, super.value);
+
+  @override
+  int get width => value.toString().length;
+
+  List<(int, int)> get adjecentFields => [
+        (x - 1, y - height),
+        (x - 1, y),
+        (x - 1, y + height),
+        ...Iterable.generate(width, (i) => (x + i, y - height)),
+        ...Iterable.generate(width, (i) => (x + i, y + height)),
+        (x + width, y - height),
+        (x + width, y),
+        (x + width, y + height),
+      ];
+
+  @override
+  String toString() {
+    return 'NumberField(x: $x, y: $y, number: $value)';
+  }
+}
+
+class SymbolField extends Field<String> {
+  SymbolField(super.x, super.y, super.value);
+
+  @override
+  int get width => value.length;
+
+  @override
+  String toString() {
+    return 'SymbolField(x: $x, y: $y, symbol: $value)';
+  }
+}
--- test/day03_test.dart
+++ test/day03_test.dart
@@ -10,36 +10,43 @@ import '../solutions/day03.dart';
 
 /// Paste in the small example that is given for the FIRST PART of the puzzle.
 /// It will be evaluated again the `_exampleSolutionPart1` below.
-/// Make sure to respect the multiline string format to avoid additional 
+/// Make sure to respect the multiline string format to avoid additional
 /// newlines at the end.
-const _exampleInput1 = '''
-''';
+const _exampleInput1 = r'''
+467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..''';
 
 /// Paste in the small example that is given for the SECOND PART of the puzzle.
 /// It will be evaluated against the `_exampleSolutionPart2` below.
 ///
 /// In case the second part uses the same example, uncomment below line instead:
-// const _exampleInput2 = _exampleInput1;
-const _exampleInput2 = '''
-''';
+const _exampleInput2 = _exampleInput1;
 
 /// The solution for the FIRST PART's example, which is given by the puzzle.
-const _exampleSolutionPart1 = 0;
+const _exampleSolutionPart1 = 4361;
 
 /// The solution for the SECOND PART's example, which is given by the puzzle.
-const _exampleSolutionPart2 = 0;
+const _exampleSolutionPart2 = 467835;
 
 /// The actual solution for the FIRST PART of the puzzle, based on your input.
 /// This can only be filled out after you have solved the puzzle and is used
 /// for regression testing when refactoring.
 /// As long as the variable is `null`, the tests will be skipped.
-const _puzzleSolutionPart1 = null;
+const _puzzleSolutionPart1 = 535078;
 
 /// The actual solution for the SECOND PART of the puzzle, based on your input.
 /// This can only be filled out after you have solved the puzzle and is used
 /// for regression testing when refactoring.
 /// As long as the variable is `null`, the tests will be skipped.
-const _puzzleSolutionPart2 = null;
+const _puzzleSolutionPart2 = 75312571;
 
 void main() {
   group(
